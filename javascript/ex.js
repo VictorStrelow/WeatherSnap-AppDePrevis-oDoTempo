04d77f2596a01f76e57db857e3a4f0047e704b8f
VICTOR DANIEL STRELOW <victor_strelow@estudante.sesisenai.org.br>
2025-12-12 21:46:08 -0300

    refactor: remocao de pastas e arquivos não utilizados

--- javascript/ex.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('searchBtn').addEventListener('click', searchCity);
-document.getElementById('cityInput').addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') searchCity();
-});
-
-renderGPList();
